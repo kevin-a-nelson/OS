d8ed9bac26a4aca650755956a1bce7aca0fe7c81
SamuraiViking <nelson67@stolaf.edu>
2020-08-28 16:34:05 -0500

    Step 3 complete

--- Lab2/echoline.c
+++ Lab2/echoline.c
@@ -1,23 +1,43 @@
+/* example of struct in C. RAB 1/2001 */
 #include <stdio.h>
-#include <stdlib.h>
-
-int input(char *s,int length);
-
-int main()
+struct name
 {
-	char *buffer;
+	int count;
+	char *input_name;
+	int status;
+};
+
+int read_name(struct name *input_name)
+{
 	size_t bufsize = 32;
 	size_t chars;
 
-	if( buffer == NULL)
+	printf("Enter a sentence: ");
+	chars = getline(&input_name->input_name, &bufsize, stdin);
+
+	if (input_name->input_name == NULL)
 	{
-		exit(1);
+		return 0;
 	}
 
-	printf("Enter a sentence: ");
-	chars = getline(&buffer,&bufsize,stdin);
-	printf("You entered: %s\n",buffer);
-	printf("Length: %zu\n", chars + 1);
+	input_name->status = 1;
 
-	return(0);
+	return 1;
+}
+
+/* main program */
+int main()
+{
+	struct name input_name;
+
+	if (!read_name(&input_name))
+	{
+		printf("Unable to read name successfully\n");
+	}
+	else
+	{
+		printf("You entered: %s\n", input_name.input_name);
+	}
+
+	return 0;
 }

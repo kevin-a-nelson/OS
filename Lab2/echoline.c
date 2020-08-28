699fbc76d0e08863497199f4e26b90a0735cda4e
SamuraiViking <nelson67@stolaf.edu>
2020-08-28 15:38:07 -0500

    Step 4 complete

--- Lab2/echoline.c
+++ Lab2/echoline.c
@@ -5,20 +5,19 @@ int input(char *s,int length);
 
 int main()
 {
-    char *buffer;
-    size_t bufsize = 32;
-    size_t chars;
+	char *buffer;
+	size_t bufsize = 32;
+	size_t chars;
 
-    // buffer = (char *)malloc(bufsize * sizeof(char));
+	if( buffer == NULL)
+	{
+	exit(1);
+	}
 
-    if( buffer == NULL)
-    {
-        exit(1);
-    }
+	printf("Enter a sentence: ");
+	chars = getline(&buffer,&bufsize,stdin);
+	printf("You entered: %s",buffer);
+	printf("Length: %zu\n", chars);
 
-    printf("Enter a sentence: ");
-    chars = getline(&buffer,&bufsize,stdin);
-    printf("You entered: %s\n",buffer);
-
-    return(0);
+	return(0);
 }

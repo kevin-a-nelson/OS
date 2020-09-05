b931f2b6c11677e71f55ac858b98303cb92eb486
SamuraiViking <nelson67@stolaf.edu>
2020-09-05 17:42:08 -0500

    read all of contents in start and add it to end

--- hw3/lseek.c
+++ hw3/lseek.c
@@ -8,16 +8,19 @@
 // Driver code
 int main()
 {
+    //
     char arr[100];
+
     // Open the file for READ only.
     int f_write = open("start.txt", O_RDONLY);
 
     // Open the file for WRITE and READ only.
     int f_read = open("end.txt", O_WRONLY);
 
+    // read all of the bytes
     while (read(f_write, arr, 1))
     {
-        lseek(f_write, 1, SEEK_CUR);
+        lseek(f_write, 0, SEEK_CUR);
         write(f_read, arr, 1);
     }
 

b6e3a4732aeb4882a1dd6a040855e4a105aaef5d
SamuraiViking <nelson67@stolaf.edu>
2020-09-05 17:34:42 -0500

    read file and print to output file

---
+++ hw3/lseek.c
@@ -0,0 +1,28 @@
+// C program to read nth byte of a file and
+// copy it to another file using lseek
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <fcntl.h>
+
+// Driver code
+int main()
+{
+    char arr[100];
+    // Open the file for READ only.
+    int f_write = open("start.txt", O_RDONLY);
+
+    // Open the file for WRITE and READ only.
+    int f_read = open("end.txt", O_WRONLY);
+
+    while (read(f_write, arr, 1))
+    {
+        lseek(f_write, 1, SEEK_CUR);
+        write(f_read, arr, 1);
+    }
+
+    close(f_write);
+    close(f_read);
+
+    return 0;
+}
